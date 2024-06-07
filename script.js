2568f333208a8a73ae99b5d82e2c6ca53b1dc17f
Vladyslav Martynenko <142616642+Vladmmar@users.noreply.github.com>
2024-06-07 13:34:31 +0300

    First stable version

---
+++ script.js
@@ -0,0 +1,39 @@
+let active = 0;
+const buttons = document.querySelectorAll("button");
+buttons.forEach((value, index, array) => {
+    value.addEventListener("click", (event) => {
+        const imagePlusCurr = document.getElementById(`image-${index + 1}-plus`)
+        const imagePlusPrev = document.getElementById(`image-${active}-plus`);
+        const imageMinusCurr = document.getElementById(`image-${index + 1}-minus`)
+        const imageMinusPrev = document.getElementById(`image-${active}-minus`);
+        const textCurr = document.getElementById(`text-${index + 1}`);
+        const textPrev = document.getElementById(`text-${active}`);
+        if(active != 0 && active != index + 1){
+            imageMinusPrev.style.display = "none";
+            imagePlusPrev.style.display = "block";
+            textPrev.style.display = "none";
+        }
+        if(imagePlusCurr.style.display == "block"){
+            imagePlusCurr.style.display = "none";
+            imageMinusCurr.style.display = "block";
+        } else{
+            imagePlusCurr.style.display = "block";
+            imageMinusCurr.style.display = "none";
+        }   
+        if(textCurr.style.display == "none"){
+            textCurr.style.display = "block";
+        } else{
+            textCurr.style.display = "none";
+        }
+        active = index + 1;
+    })
+    value.addEventListener("mouseover", (event) => {
+        console.log("over")
+        console.log(document.getElementById(`header-${index + 1}`))
+        document.getElementById(`header-${index + 1}`).style.color = "rgb(174, 40, 235)" /*"hsl(292, 42%, 14%)"*/;
+    })
+    value.addEventListener("mouseout", (event) => {
+        console.log("out")
+        document.getElementById(`header-${index + 1}`).style.color = "black";
+    })
+})
